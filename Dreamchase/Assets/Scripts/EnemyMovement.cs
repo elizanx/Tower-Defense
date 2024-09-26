8ae6e794885de05e029918d958a119bd29b49e9c
eliza <elizanabibaks@gmail.com>
2024-09-26 22:27:36 +0200

    sprite walking

--- Dreamchase/Assets/Scripts/EnemyMovement.cs
+++ Dreamchase/Assets/Scripts/EnemyMovement.cs
@@ -6,17 +6,25 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField]
-    
+    public GameObject ghost;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    public float speed;
+    private void Start()
     {
+
+    }
+
+    private void Update()
+    {
+        Movement();
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Movement()
     {
-        
+        Vector3 Walk = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        ghost.transform.position += Walk * speed * Time.deltaTime;
     }
 }
